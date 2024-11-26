5df331f93245fca3aa728220bacc6a06feb2b922
ajdude9 <adamtheteeth@hotmail.com>
2024-11-26 18:02:11 +0000

    Fixed all errors in project and made code more efficient.

--- Assets/CongratScript.cs
+++ Assets/CongratScript.cs
@@ -1,50 +1,69 @@
 ﻿using System.Collections;
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class CongratScript : MonoBehaviour
 {
-    public TextMesh Text;
-    public ParticleSystem SparksParticles;
-    
-    private List<string> TextToDisplay;
-    
-    private float RotatingSpeed;
-    private float TimeToNextText;
+    private TextMesh text;
+    public ParticleSystem sparksParticles;
+    private List<string> textToDisplay;
+    private int currentText;
+    private bool textSwitch;
 
-    private int CurrentText;
-    
     // Start is called before the first frame update
     void Start()
     {
-        TimeToNextText = 0.0f;
-        CurrentText = 0
-        
-        RotatingSpeed = 1.0;
-
-        TextToDisplay.Add("Congratulation");
-        TextToDisplay.Add("All Errors Fixed");
-
-        Text.text = TextToDisplay[0];
-        
-        SparksParticles.Play();
+        textToDisplay = new List<string>();
+        text = GameObject.Find("TextDisplay").GetComponent<TextMesh>();
+        currentText = 0;
+        textSwitch = true;
+        textToDisplay.Add("Congratulations");
+        textToDisplay.Add("All Errors Fixed");
+        text.text = textToDisplay[0];
+        sparksParticles.Play();
+        InvokeRepeating("changeText", 0, 1.5f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void changeText()
     {
-        TimeToNextText += Time.deltaTime;
-
-        if (TimeToNextText > 1.5f)
+        switch (textSwitch)
         {
-            TimeToNextText = 0.0f;
-            
-            CurrentText++;
-            if (CurrentText >= TextToDisplay.Count)
-            {
-                CurrentText = 0;
-
-
-            Text.text = TextToDisplay[CurrentText];
+            case false:
+                currentText = 1;
+                text.text = textToDisplay[currentText];
+                textSwitch = !textSwitch;
+                break;
+            case true:
+                currentText = 0;
+                text.text = textToDisplay[currentText];
+                textSwitch = !textSwitch;
+                break;
         }
     }
 }
+
+/**
+CODE ERRORS:
+
+- Missing bracket to close the update method
+- Rotating Speed value lacking 'f' to define as float
+- Missing semicolon after currentText
+- Variables are not written in camel case
+- Failed to define 'using System.Collections.Generic'; causing error with List variable type
+- 'Congratulation' is incorrect grammar; should be 'Congratulations'
+- textToDisplay is not defined
+- currentText sets its value before its value is used in the if statement; causing text to not change
+- Text switching is done in an inefficient manner; switched to boolean variable to determine what text to be displayed and changed if statement to switch
+- Using the update method to run a counter is inefficient; created seperate function and InvokeRepeating method to repeat it from the start.
+- timeToNextText is unnecessary and inefficient with InvokeRepeating.
+- rotatingSpeed is unused and thus unnecessary.
+- Update() is unused and thus unnecessary.
+
+SCENE ERRORS:
+
+- Text object not assigned to text display
+- Particle object not assigned to text display
+- Script assigned to the text object itself instead of an empty object
+
+*/
